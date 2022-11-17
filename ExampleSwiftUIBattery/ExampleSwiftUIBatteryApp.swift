cd0b24fbb1fb2ee51b803931a283a9eb4f1812de
Samuel Rodriguez-Lozano <samuel@lovebox.love>
2022-11-17 19:13:47 +0100

    Initial Commit

---
+++ ExampleSwiftUIBattery/ExampleSwiftUIBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIBatteryApp.swift
+//  ExampleSwiftUIBattery
+//
+//  Created by Samuel Rodriguez-Lozano on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
